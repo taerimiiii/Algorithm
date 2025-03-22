a83cdfaa6791daace6d275965c37abacd6f7206d
taerimiiii <shipton0201@gmail.com>
2025-03-22 14:58:39 +0900

    [Bronze III] Title: 369, Time: 992 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/17614. 369/369.py
+++ 백준/Bronze/17614. 369/369.py
@@ -1,20 +1,10 @@
-def get_digit(number) :
-    if number < 10 :
-        digit_list.append(number)
-        return
-    digit_list.append(number % 10)
-    number = number // 10
-    get_digit(number)
-
-
 n = int(input())
+answer = ['3', '6', '9']
 
 cnt = 0
-for number in range(1, n + 1) :
-    digit_list = []
-    get_digit(number)
-    for i in range(len(digit_list)) :
-        if digit_list[i] == 3 or digit_list[i] == 6 or digit_list[i] == 9:
+for string in range(1, n + 1) :
+    number = str(string)
+    for digit in number :
+        if digit in answer :
             cnt += 1
-
 print(cnt)
