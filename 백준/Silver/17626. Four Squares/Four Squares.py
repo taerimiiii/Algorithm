2dac2fbe7805229613a6abfc2a9e5eae565e83fd
taerimiiii <shipton0201@gmail.com>
2025-03-23 12:13:29 +0900

    [Silver III] Title: Four Squares, Time: 64 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.py
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -1,13 +1,17 @@
+# 완전탐색 풀이
 n = int(input())
+arr = [0 if i ** 0.5 % 1 else 1 for i in range(n + 1)] # 제곱수는 1로 저장
 
-dp = [0, 1]
-
-for i in range(2, n + 1):
-    min_ = 4
-    j = 1
-    while (j ** 2) <= i:
-        min_ = min(min_, dp[i - j**2])
-        j += 1
-    dp.append(min_ + 1)
-    
-print(dp[n])
+min_val = 4
+for i in range(int(n ** 0.5), 0, -1):
+    if arr[n] : # n이 제곱수일 경우
+        min_val=1
+        break
+    elif arr[n - i ** 2] : # 나머지가 제곱수일 경우
+        min_val=2
+        break
+    else:
+        for j in range(int((n - i ** 2) ** 0.5), 0, -1):
+            if arr[(n  -i ** 2) - j ** 2]: # 제곱수를 한번 더 뺀 나머지가 제곱수일 경우
+                min_val=3
+print(min_val)
