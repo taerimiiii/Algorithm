495fb80d31f4ebb880d0baad5908588391595a0f
taerimiiii <shipton0201@gmail.com>
2025-04-12 13:09:44 +0900

    [Silver IV] Title: 제곱근, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/13706. 제곱근/제곱근.py
@@ -0,0 +1,16 @@
+n = int(input())
+left = 1
+right = n
+
+while 1:
+    mid = (left + right) // 2
+    
+    if mid ** 2 == n:
+        print(mid)
+        break
+    
+    elif mid ** 2 > n:
+        right = mid - 1
+        
+    elif mid ** 2 < n:
+        left = mid + 1
