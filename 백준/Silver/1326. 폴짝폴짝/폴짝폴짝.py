3829fc2b31af8a17dfd8450b01c3761ba6675979
taerimiiii <shipton0201@gmail.com>
2025-05-11 13:58:27 +0900

    [Silver II] Title: 폴짝폴짝, Time: 60 ms, Memory: 34944 KB -BaekjoonHub

---
+++ 백준/Silver/1326. 폴짝폴짝/폴짝폴짝.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+def bfs():
+    queue = deque([a - 1])
+    visited = [-1] * 100000
+    visited[a - 1] = 0
+
+    while queue:
+        target = queue.popleft()
+
+
+        for i in range(target, n, m[target]):
+            if visited[i] == -1:
+                queue.append(i)
+                visited[i] = visited[target] + 1
+
+                if i == b - 1:
+                    return visited[i]
+
+        for i in range(target, -1, -m[target]):
+            if visited[i] == -1:
+                queue.append(i)
+                visited[i] = visited[target] + 1
+                if i == b - 1:
+                    return visited[i]
+
+    return -1
+
+
+n = int(input())
+m = list(map(int, input().split()))
+a, b = map(int, input().split())
+print(bfs())
