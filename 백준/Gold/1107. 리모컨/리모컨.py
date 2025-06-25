a629caca59303cae573bf5933992e696da8aa6c4
taerimiiii <shipton0201@gmail.com>
2025-06-25 23:19:40 +0900

    [Gold IV] Title: 리모컨, Time: 2564 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Gold/1107. 리모컨/리모컨.py
@@ -0,0 +1,23 @@
+from sys import stdin
+input = stdin.readline
+
+target = int(input())
+n = int(input())
+if n == 0 :
+    broken = []
+else :
+    broken = list(map(int, input().split()))
+
+min_count = abs(100 - target)
+
+for nums in range(1000001):
+    nums = str(nums)
+    
+    for j in range(len(nums)):
+        if int(nums[j]) in broken:
+            break
+
+        elif j == len(nums) - 1:
+            min_count = min(min_count, abs(int(nums) - target) + len(nums))
+
+print(min_count)
