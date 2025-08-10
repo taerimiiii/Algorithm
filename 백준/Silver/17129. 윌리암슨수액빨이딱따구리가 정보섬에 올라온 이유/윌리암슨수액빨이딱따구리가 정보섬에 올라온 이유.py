4061d2b2b0e059b9c7cb437a3a9ecf5ee3837a31
taerimiiii <shipton0201@gmail.com>
2025-08-10 12:50:06 +0900

    [Silver I] Title: 윌리암슨수액빨이딱따구리가 정보섬에 올라온 이유, Time: 3732 ms, Memory: 514852 KB -BaekjoonHub

---
+++ 백준/Silver/17129. 윌리암슨수액빨이딱따구리가 정보섬에 올라온 이유/윌리암슨수액빨이딱따구리가 정보섬에 올라온 이유.py
@@ -0,0 +1,43 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+def bfs(start_row, start_col):
+    queue = deque()
+    d_row = [0, 1, 0, -1]
+    d_col = [1, 0, -1, 0]
+
+    queue.append([start_row, start_col])
+    board[start_row][start_col] = "1"
+
+    distance = 1
+    while queue:
+        for _ in range(len(queue)):
+            now_row, now_col = queue.popleft()
+            for i in range(4):
+                next_row, next_col = now_row + d_row[i], now_col + d_col[i]
+                if 0 <= next_row < n and 0 <= next_col < m and board[next_row][next_col] != "1":
+                    if board[next_row][next_col] in ("3", "4", "5"):
+                        print("TAK")
+                        print(distance)
+                        return
+                    else:
+                        queue.append([next_row, next_col])
+                        board[next_row][next_col] = "1"
+        distance += 1
+
+
+    print("NIE")
+
+
+if __name__ == "__main__":
+    n, m = map(int, input().split())
+    board = []
+    for row in range(n):
+        now = list(input())
+        board.append(now)
+        for col in range(m):
+            if now[col] == "2":
+                start_row, start_col = row, col
+
+    bfs(start_row, start_col)
