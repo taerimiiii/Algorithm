944eb3f61c4c134a6496ebab37b2dfabbfc0f526
taerimiiii <shipton0201@gmail.com>
2025-09-23 14:44:58 +0900

    [Silver I] Title: 숨바꼭질, Time: 116 ms, Memory: 35240 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,64 +1,39 @@
 import sys
 input = sys.stdin.readline
 
-def back_tracking(arr, target, sec, is_in) :
-    locations = []
-    sec += 1
-    for elem in arr :
-        a, b, c = elem + 1, elem - 1, elem * 2
-        if a == target or b == target or c == target:
-            return sec
-        if not is_in[a] :
-            locations.append(a)
-        if not is_in[b] :
-            locations.append(b)
-        
-        locations.append(c)
-    print(locations, sec)
-
-    back_tracking(locations, target, sec)
-
 n, k = map(int, input().split())
 
 search = [n]
 sec = 0
-is_in = [False] * 100001
+visited = [False] * 100001
 
-success = True
+# 이동 처리 함수
+def move(pos, nxt, locations):
+    if 0 <= nxt <= 100000 and not visited[nxt]:
+        if nxt == k:
+            return True
+        visited[nxt] = True
+        locations.append(nxt)
+    return False
 
-if n == k :
-    success = False
-
-while success :
-    locations = []
-    sec += 1
-    for elem in search :
-        if 0 <= elem + 1 <= 100000 :
-            x = elem + 1
-            if x == k :
+if n == k:
+    print(0)
+else:
+    success = True
+    while success:
+        locations = []
+        sec += 1
+        for elem in search:
+            # 세 가지 이동을 함수로 처리
+            if move(elem, elem + 1, locations): 
                 success = False
                 break
-            if not is_in[x] :
-                locations.append(x)
-                is_in[x] = True
-
-        if 0 <= elem - 1 <= 100000 :
-            x = elem - 1
-            if x == k :
+            if move(elem, elem - 1, locations): 
                 success = False
                 break
-            if not is_in[x] :
-                locations.append(x)
-                is_in[x] = True
-
-        if 0 <= elem * 2 <= 100000 :
-            x = elem * 2
-            if x == k :
+            if move(elem, elem * 2, locations): 
                 success = False
                 break
-            if not is_in[x] :
-                locations.append(x)
-                is_in[x] = True
-    search = locations
+        search = locations
 
-print(sec)
+    print(sec)
