9ff2d883f5d43297e0525c2f857b5ed2f5bb7432
taerimiiii <shipton0201@gmail.com>
2025-10-03 19:19:14 +0900

    [Gold V] Title: Z, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.py
+++ 백준/Gold/1074. Z/Z.py
@@ -1,27 +1,29 @@
 import sys
 input = sys.stdin.readline
 
+def z(n, r, c):
+    # n: 현재 2^n 크기의 정사각형
+    if n == 0:
+        return 0
+
+    half = 2 ** (n - 1)
+    size = half * half
+
+    # 1사분면 (좌상단)
+    if r < half and c < half:
+        return z(n - 1, r, c)
+
+    # 2사분면 (우상단)
+    elif r < half and c >= half:
+        return size + z(n - 1, r, c - half)
+
+    # 3사분면 (좌하단)
+    elif r >= half and c < half:
+        return 2 * size + z(n - 1, r - half, c)
+
+    # 4사분면 (우하단)
+    else:
+        return 3 * size + z(n - 1, r - half, c - half)
+
 n, r, c = map(int, input().split())
-
-cnt = 0
-n = 2 ** n
-
-while n > 1 :
-	n //= 2
-
-	if r < n and c < n :
-		cnt += 0
-
-	elif r < n and c >= n : 
-		cnt += n * n * 1
-		c -= n
-        
-	elif r >= n and c < n : 
-		cnt += n * n * 2
-		r -= n
-        
-	else:
-		cnt += n * n * 3
-		r, c = r - n, c - n
-    
-print(cnt)
+print(z(n, r, c))
