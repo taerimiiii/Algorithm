cf60308da9b5b8b252beb82593eb70fb4b9819ef
Taerim <shipton0201@gmail.com>
2026-02-27 19:24:42 +0900

    [Gold V] Title: 치킨 배달, Time: 224 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -1,6 +1,24 @@
 import sys
 input = sys.stdin.readline
-from itertools import combinations
+
+def dfs(depth, idx) :
+    global answer
+
+    if depth == m :
+        sum_val = 0
+        for house in houses :
+            min_val = sys.maxsize
+            for select_chicken in select_chickens :
+                min_val = min(abs(select_chicken[0] - house[0]) + abs(select_chicken[1] - house[1]), min_val)
+            sum_val += min_val
+        answer = min(sum_val, answer)
+        return
+
+    for i in range(idx, len(chickens)) :
+        select_chickens.append(chickens[i])
+        dfs(depth + 1, i + 1)
+        select_chickens.pop()
+
 
 n, m = map(int, input().split())
 
@@ -10,20 +28,11 @@ for i in range(n) :
     arr = list(map(int, input().split()))
     for j in range(n) :
         if arr[j] == 1 :
-            houses.append([i, j])
+            houses.append((i, j))
         elif arr[j] == 2 :
-            chickens.append([i, j])
+            chickens.append((i, j))
 
-
-combi = combinations(chickens, m)
+select_chickens = []
 answer = sys.maxsize
-for chicken in combi :
-    sum_val = 0
-    for house in houses :
-        min_val = sys.maxsize
-        for i in range(m) :
-            min_val = min(abs(chicken[i][0] - house[0]) + abs(chicken[i][1] - house[1]), min_val)
-        sum_val += min_val
-    answer = min(sum_val, answer)
-
+dfs(0, 0)
 print(answer)
