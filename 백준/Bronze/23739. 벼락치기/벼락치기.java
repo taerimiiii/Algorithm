8ca9af2b083b50c05b5ed4dce0fed60da1cd967a
Taerim <shipton0201@gmail.com>
2026-03-16 20:39:47 +0900

    [Bronze I] Title: 벼락치기, Time: 96 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/23739. 벼락치기/벼락치기.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 입력 처리
+        int n = Integer.parseInt(br.readLine());
+
+        int answer = 0; // 답을 저장하는 변수
+        int time = 30;  // 남은 시간
+
+        for (int i = 0; i < n; i++) {
+            int chap = Integer.parseInt(br.readLine());
+
+            // 공부하는데 걸리는 시간이 남은 시간보다 작거나 같을 경우
+            // 성공적으로 공부하므로 답+1
+            if (chap <= time) {
+                time -= chap;
+                answer += 1;
+            }
+            // 공부할 게 남았지만 덮어버리는 경우
+            else {
+                // 챕터의 절반이 시간 안에 공부한 양을 넘을 경우
+                // 잘 기억했으므로 답+1
+                if (chap / 2 >= (chap - time)) {
+                    answer += 1;
+                }
+                time = 0;
+            }
+
+            // 남은 시간이 0 이라면 30분 휴식을 취하고, 다시 시간이 30분으로 늘어남.
+            if (time == 0) {
+                time = 30;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
