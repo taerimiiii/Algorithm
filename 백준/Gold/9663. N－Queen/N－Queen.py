755bc35e0764e14745bed98adc6e73c7128ec094
Taerim <shipton0201@gmail.com>
2026-03-30 00:15:15 +0900

    [Gold IV] Title: N-Queen, Time: 28576 ms, Memory: 207604 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,22 +1,26 @@
-n = int(input())
+import sys
+input=sys.stdin.readline
 
-arr = [0] * n
-
-def is_possible(curr_row):
-    for front_row in range(curr_row):
-        if arr[curr_row] == arr[front_row] or abs(curr_row - front_row) == abs(arr[curr_row] - arr[front_row]):
+def is_success(curr_row) :
+    for front_row in range(curr_row) :
+        if arr[front_row] == arr[curr_row] or curr_row - front_row == abs(arr[curr_row] - arr[front_row]) :
             return False
-    return True 
+    return True
 
-def dfs(row):
-    if row == n:
+def dfs(row) :
+    if row == n :
         return 1
     
     ans = 0
-    for col in range(n):
-        arr[row] = col #[row, col]에 퀸 두기
-        if is_possible(row):
-            ans += dfs(row + 1)
+    for col in range(n) :
+        arr[row] = col
+        if is_success(row) :
+            ans += dfs(row+1)
+
     return ans
+
+n = int(input())
+
+arr = [0] * n
 
 print(dfs(0))
