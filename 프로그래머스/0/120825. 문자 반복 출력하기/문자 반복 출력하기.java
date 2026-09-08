84466df6274b85e49c64475042ac848d66b177d5
Taerim <shipton0201@gmail.com>
2026-09-08 14:13:00 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.03 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String my_string, int n) {
+        StringBuilder answer = new StringBuilder();
+        
+        for (int i = 0; i < my_string.length(); i++) {
+            char c = my_string.charAt(i);
+            
+            for (int j = 0; j < n; j++) {
+                answer.append(c);
+            }
+        }
+        
+        return answer.toString();
+    }
+}
